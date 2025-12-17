b42f241da73a39cfb414a3cfefaec9a6f57377a9
Rohit Parit <rohitparit1934@gmail.com>
2025-12-17 22:31:40 +0530

    Add main Spring Boot application class

---
+++ src/main/java/com/rps/smartsplit/SmartSplitApplication.java
@@ -0,0 +1,13 @@
+package com.rps.smartsplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSplitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartSplitApplication.class, args);
+    }
+
+}
