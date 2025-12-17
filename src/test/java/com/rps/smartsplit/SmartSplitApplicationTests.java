efeaa84535c7523410e774e0b458a02214d6b209
Rohit Parit <rohitparit1934@gmail.com>
2025-12-17 22:33:56 +0530

    Add basic application tests

---
+++ src/test/java/com/rps/smartsplit/SmartSplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rps.smartsplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSplitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
