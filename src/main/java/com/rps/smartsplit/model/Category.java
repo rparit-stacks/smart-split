eac0515dfad0a62b1a2d042c6e888030f165de48
Rohit Parit <rohitparit1934@gmail.com>
2025-12-17 22:32:02 +0530

    Add Category model for expense categorization

---
+++ src/main/java/com/rps/smartsplit/model/Category.java
@@ -0,0 +1,77 @@
+package com.rps.smartsplit.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    @OneToMany(mappedBy = "category")
+    private List<Expense> expenses;
+
+
+    private String name;
+    private String description;
+    private String color;
+    private Instant createdAt;
+
+    public Category() {
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(List<Expense> expenses) {
+        this.expenses = expenses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+}
