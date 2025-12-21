cc8560db293752b03ac142b44652ef01ccc4e0b4
Rohit Parit <rohitparit1934@gmail.com>
2025-12-21 22:37:35 +0530

    feat: Create CategoryUpdateRequest DTO for category updates

--- src/main/java/com/rps/smartsplit/dto/CategoryUpdateRequest.java
+++ src/main/java/com/rps/smartsplit/dto/CategoryUpdateRequest.java
@@ -1,4 +1,55 @@
 package com.rps.smartsplit.dto;
 
+import java.util.UUID;
+
 public class CategoryUpdateRequest {
+    private UUID id;
+    private String name;
+    private String description;
+    private String color;
+    private UUID adminId;
+
+    public CategoryUpdateRequest() {
+    }
+
+    public UUID getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(UUID adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public java.util.UUID getId() {
+        return id;
+    }
+
+    public void setId(java.util.UUID id) {
+        this.id = id;
+    }
+
 }
