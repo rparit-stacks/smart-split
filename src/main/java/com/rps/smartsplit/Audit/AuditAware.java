1d3fd8cc54accfd7db2b7cc27d24d91f494209c1
Rohit Parit <rohitparit1934@gmail.com>
2025-12-21 22:35:05 +0530

    feat: Make AuditAware bean name explicit for JPA auditing

--- src/main/java/com/rps/smartsplit/Audit/AuditAware.java
+++ src/main/java/com/rps/smartsplit/Audit/AuditAware.java
@@ -1,4 +1,23 @@
 package com.rps.smartsplit.Audit;
 
-public class AuditAware {
+import com.rps.smartsplit.model.User;
+import com.rps.smartsplit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service("auditAware")
+public class AuditAware implements AuditorAware<User> {
+
+    @Autowired
+    private UserService userService;
+
+
+
+    @Override
+    public Optional<User> getCurrentAuditor() {
+        return Optional.ofNullable(userService.getLoggedInUser());
+    }
 }
