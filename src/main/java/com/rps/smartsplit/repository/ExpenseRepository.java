0440c64e60ec37b8c7fdf05f616618b936071b69
Rohit Parit <rohitparit1934@gmail.com>
2025-12-21 22:37:49 +0530

    refactor: Update ExpenseRepository with new query methods

--- src/main/java/com/rps/smartsplit/repository/ExpenseRepository.java
+++ src/main/java/com/rps/smartsplit/repository/ExpenseRepository.java
@@ -1,5 +1,6 @@
 package com.rps.smartsplit.repository;
 
+import com.rps.smartsplit.model.Category;
 import com.rps.smartsplit.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,18 @@ import java.util.UUID;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
+    /**
+     * Check if any expenses exist for the given category
+     * @param category The category to check
+     * @return true if expenses exist, false otherwise
+     */
+    boolean existsByCategory(Category category);
+    
+    /**
+     * Count expenses for a given category
+     * @param category The category to count expenses for
+     * @return number of expenses using this category
+     */
+    long countByCategory(Category category);
 }
 
